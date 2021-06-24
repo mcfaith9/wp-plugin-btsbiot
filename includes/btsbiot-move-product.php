2298af968fe9370dbd20d3dace33e480f7f2e819
mcfaith9 <marclouie.cabigas@yahoo.com>
2021-06-24 15:55:04 +0800

    fix swap files for product and post

--- includes/btsbiot-move-product.php
+++ includes/btsbiot-move-product.php
@@ -1,9 +1,18 @@
 <?php 
+/**
+ * Move/Copy Product for Woocommerce
+ *
+ * @link       aguyiknow.com.au
+ * @since      1.0.1
+ *
+ * @package    Btsbiot
+ * @subpackage Btsbiot/includes
+*/
 
-// Mcfaith code for bulk copy Start
-add_filter( 'bulk_actions-edit-post', 'mcfaith_my_bulk_multisite_actions' );
 
-function mcfaith_my_bulk_multisite_actions( $bulk_array ) {
+add_filter( 'bulk_actions-edit-product', 'bulk_action_move_product' );
+
+function bulk_action_move_product( $bulk_array ) {
  	if( $sites = get_sites( array(
 		// 'site__in' => array( 1,2,3 )
 		'site__not_in' => get_current_blog_id(), // excluding current blog
@@ -15,7 +24,7 @@ function mcfaith_my_bulk_multisite_actions( $bulk_array ) {
 	return $bulk_array;
 }
 
-add_filter( 'handle_bulk_actions-edit-post', 'mcfaith_bulk_action_multisite_handler', 10, 3 );
+add_filter( 'handle_bulk_actions-edit-product', 'mcfaith_bulk_action_multisite_handler', 10, 3 );
 
 function mcfaith_bulk_action_multisite_handler( $redirect, $doaction, $object_ids ) {
 
@@ -26,14 +35,14 @@ function mcfaith_bulk_action_multisite_handler( $redirect, $doaction, $object_id
 	if( strpos( $doaction, "move_to_" ) === 0 ) {
 		$blog_id = str_replace( "move_to_", "", $doaction );
 
-		foreach ( $object_ids as $post_id ) {
+		foreach ( $object_ids as $product_id ) {
 
 			// get the original post object as an array
-			$post = get_post( $post_id, ARRAY_A );
+			$post = get_post( $product_id, ARRAY_A );
 			// if you need to apply terms (more info below the code)
-			$post_terms = wp_get_object_terms($post_id, 'category', array('fields' => 'slugs'));
+			$product_terms = wp_get_object_terms($product_id, 'category', array('fields' => 'slugs'));
 			// get all the post meta
-			$data = get_post_custom($post_id);
+			$data = get_post_custom($product_id);
 			// empty ID field, to tell WordPress to create a new post, not update an existing one
 			$post['ID'] = '';
 
@@ -41,15 +50,15 @@ function mcfaith_bulk_action_multisite_handler( $redirect, $doaction, $object_id
 			switch_to_blog( $blog_id );
 			
 			// insert the post
-			$inserted_post_id = wp_insert_post($post); // insert the post
+			$inserted_product_id = wp_insert_post($post); // insert the post
 			// update post terms
-			wp_set_object_terms($inserted_post_id, $post_terms, 'category', false);
+			wp_set_object_terms($inserted_product_id, $product_terms, 'category', false);
 			$taxonomies = get_object_taxonomies( $post['post_type'] );
 			foreach ( $taxonomies as $taxonomy ) {
-				$post_terms = wp_get_object_terms( $post_id, $taxonomy, array('fields' => 'slugs') );
+				$product_terms = wp_get_object_terms( $product_id, $taxonomy, array('fields' => 'slugs') );
 			}
 			foreach ( $taxonomies as $taxonomy ) {
-				wp_set_object_terms( $inserted_post_id, $post_terms, $taxonomy, false );
+				wp_set_object_terms( $inserted_product_id, $product_terms, $taxonomy, false );
 			}
 			// add post meta
 			foreach ( $data as $key => $values) {
@@ -58,20 +67,16 @@ function mcfaith_bulk_action_multisite_handler( $redirect, $doaction, $object_id
 					continue;
 				}
 				foreach ($values as $value) {
-					add_post_meta( $inserted_post_id, $key, $value );
+					add_post_meta( $inserted_product_id, $key, $value );
 				}
 			}
 
 			restore_current_blog();
-
-			// if you want to copy posts, comment this line
-			//wp_delete_post( $post_id );
-
 		}
 
 
 		$redirect = add_query_arg( array(
-			'mcfaith_posts_moved' => count( $object_ids ),
+			'mcfaith_product_moved' => count( $object_ids ),
 			'mcfaith_blogid' => $blog_id
 		), $redirect );
 
@@ -80,9 +85,9 @@ function mcfaith_bulk_action_multisite_handler( $redirect, $doaction, $object_id
 }
 
 // Notice
-add_action( 'admin_notices', 'mcfaith_bulk_multisite_notices' );
+add_action( 'admin_notices', 'product_move_notice' );
 
-function mcfaith_bulk_multisite_notices() {
+function product_move_notice() {
 
 	if( ! empty( $_REQUEST['mcfaith_posts_moved'] ) ) {
 
@@ -91,7 +96,7 @@ function mcfaith_bulk_multisite_notices() {
 
 		// depending on ho much posts were changed, make the message different
 		printf( '<div id="message" class="updated notice is-dismissible"><p>' .
-			_n( '%d post has been Moved/Copy to &quot;%s&quot;.', '%d posts have been Moved/Copy to &quot;%s&quot;.', intval( $_REQUEST['mcfaith_posts_moved'] )
+			_n( '%d Product has been Moved/Copy to &quot;%s&quot;.', '%d Product have been Moved/Copy to &quot;%s&quot;.', intval( $_REQUEST['mcfaith_posts_moved'] )
 		) . '</p></div>', intval( $_REQUEST['mcfaith_posts_moved'] ), $blog->blogname );
 	}
 }
