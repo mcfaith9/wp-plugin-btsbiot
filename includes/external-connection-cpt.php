80911ad161ff76d84b15621f7727300d43d6518e
mcfaith9 <marclouie.cabigas@yahoo.com>
2021-06-30 14:17:56 +0800

    minor fix

--- includes/external-connection-cpt.php
+++ includes/external-connection-cpt.php
@@ -649,7 +649,7 @@ function screen_option() {
 function add_menu_item() {
 	$hook = add_menu_page(
 		'MCMultisite',
-		'MCMultisite',
+		'Distributor',
 		/**
 		 * Filter Distributor capabilities allowed to view external connections.
 		 *
